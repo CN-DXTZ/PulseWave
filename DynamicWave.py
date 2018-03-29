66402199ffa35b65466b103c8bc3102966471a28
CN-DXTZ <dengbin1213@gmail.com>
2018-03-29 10:11:40 +0800

    MySQL数据动态显示

--- DynamicWave.py
+++ DynamicWave.py
@@ -1,20 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import MySQL
 
 
-def display(wave):
+class Scope(object):
+    def __init__(self, ax, cur, rang=400, time=0):
+        self.rang = rang
+        self.ax = ax
+        self.cur = cur
+        self.time = time
+        self.xdata = np.arange(self.rang)
+        self.ydata = np.array(np.zeros(self.rang))
+        self.line, = ax.plot(self.xdata, self.ydata)
+        self.ax.set_xlim(0, self.rang)
+
+    def update(self, y):
+        self.ydata = self.ydata[1:]
+        sql = 'select * from wavedata where time>%s limit 1' % (self.time)
+        self.cur.execute(sql)
+        data = self.cur.fetchall()[0]
+        self.time = data[0]
+        self.ydata = np.append(self.ydata, [data[1]], axis=0)
+        self.ax.set_ylim(self.ydata.min(), self.ydata.max())
+        self.line.set_ydata(self.ydata)
+        return self.line,
+
+
+def display():
     fig, ax = plt.subplots()
-    x = np.arange(400)
-    y = np.zeros(400)
     plt.xticks([])
-    line, = ax.plot(x, y)
-    ax.set_xlim(0, 400)
-    ax.set_ylim(np.min(wave), np.max(wave))
-
-    def update(frames):
-        line.set_ydata(wave[x + frames])
-        return line,
-
-    ani = animation.FuncAnimation(fig, update, interval=2, blit=True)
+    cur = MySQL.MysqlInit()
+    scope = Scope(ax, cur, 1000)
+    ani = animation.FuncAnimation(fig, scope.update, interval=5, blit=True)
     plt.show()
+
+
+if __name__ == '__main__':
+    display()
