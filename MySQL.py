adca7298d57cf4378656e95fa621688417db6426
CN-DXTZ <2924380441@qq.com>
2019-04-03 15:23:54 +0800

    clean

--- MySQL.py
+++
@@ -1,7 +0,0 @@
-import MySQLdb as mysql
-
-def MysqlInit():
-    db = mysql.connect(user="root", passwd="1234", db="wave", host="localhost")
-    db.autocommit(True)
-    cur = db.cursor()
-    return cur
